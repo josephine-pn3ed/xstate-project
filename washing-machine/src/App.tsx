c4f13c65f6b62395637b8469beee5bb5552f86fc
jrepollo <jrepollo@dnamicro.git>
2021-07-21 05:46:47 +0800

    added ui

--- washing-machine/src/App.tsx
+++ washing-machine/src/App.tsx
@@ -1,12 +1,33 @@
 import "./App.css";
 import React from "react";
-import { Container, Button } from "@material-ui/core";
+import { Grid, Container, Button, TextField } from "@material-ui/core";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Container maxWidth="sm">
-        <Button variant="outlined">Hello</Button>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField fullWidth label="Water" variant="outlined" />
+            {/* <Button fullWidth variant="outlined">
+              LOAD[water|laundry|laundry soap]
+            </Button> */}
+            <TextField fullWidth label="Laundry" variant="outlined" />
+            <TextField fullWidth label="Laundry Soap" variant="outlined" />
+            <TextField fullWidth label="Timer" variant="outlined" />
+          </Grid>
+          HEHE, I don't know unsa akong i design mga bess.
+          <Grid item xs={12}>
+            <Button fullWidth variant="outlined">
+              LOAD[water|laundry]
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Button fullWidth variant="outlined">
+              LOAD[water]
+            </Button>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   );
